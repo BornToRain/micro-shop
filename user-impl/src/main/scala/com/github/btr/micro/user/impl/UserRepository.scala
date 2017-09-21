483dbc119b5a9cfcb54a3308d5be1ef3275d1086
BornToRain <1159930219@qq.com>
2017-09-21 18:52:58 +0800

    用户表部分还有问题 主要是UserType UDT之间的问题

--- user-impl/src/main/scala/com/github/btr/micro/user/impl/UserRepository.scala
+++ user-impl/src/main/scala/com/github/btr/micro/user/impl/UserRepository.scala
@@ -3,18 +3,34 @@ package com.github.btr.micro.user.impl
 
 import akka.Done
 import com.datastax.driver.core.PreparedStatement
+import com.github.btr.micro.user.api.Info
 import com.lightbend.lagom.scaladsl.persistence.cassandra.{CassandraReadSide, CassandraSession}
 import com.lightbend.lagom.scaladsl.persistence.{EventStreamElement, ReadSideProcessor}
+import org.joda.time.DateTime
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
+import scala.collection.JavaConversions._
+import scala.collection._
 /**
 	* 用户仓储 - 读边
 	*/
 class UserRepository(session: CassandraSession)(implicit ex: ExecutionContext)
 {
-
+	def getUsers =
+	{
+		session.selectAll(
+			"""
+				SELECT * FROM user
+			""").map(_.map(d =>
+		{
+			val name = d.getUDTValue("name")
+			val fullName = Name(name.getString("first_name"),name.getString("last_name"),Option(name.getString("en_name")))
+			Info(d.getString("id"),d.getString("mobile"),Some(fullName),Some(name.getInt("age")),addresses)
+		}))
+	}
 }
+
 
 /**
 	* 用户事件处理
@@ -22,16 +38,15 @@ class UserRepository(session: CassandraSession)(implicit ex: ExecutionContext)
 class UserReadSideProcessor(session: CassandraSession, readSide: CassandraReadSide)(implicit ec: ExecutionContext)
 extends ReadSideProcessor[UserEvt]
 {
-
-	private var insertUserPre   : PreparedStatement = _
-	private var deleteUserPre   : PreparedStatement = _
-	private var updateAddressPre: PreparedStatement = _
+	private val deleteUserPro    = Promise[PreparedStatement]
+	private val insertUserPro    = Promise[PreparedStatement]
+	private val updateAddressPro = Promise[PreparedStatement]
 
 	override def aggregateTags = UserEvt.tag.allTags
 
-	override def buildHandler() = readSide.builder[UserEvt]("userEvtOffSet")
+	override def buildHandler = readSide.builder[UserEvt]("userEvtOffSet")
 	.setGlobalPrepare(() => createTable)
-	.setPrepare(_ => fSQL)
+	.setPrepare(_ => fSQLs)
 	.setEventHandler[Created](insertUser)
 	.setEventHandler[Deleted.type](deleteUser)
 	.setEventHandler[CreatedAddress](updateAddress)
@@ -41,7 +56,7 @@ extends ReadSideProcessor[UserEvt]
 	//数据库表创建
 	private def createTable = for
 	{
-		//用户姓名表
+	//用户姓名表
 		_ <- session.executeCreateTable(
 			"""
 			CREATE TYPE IF NOT EXISTS full_name
@@ -52,7 +67,7 @@ extends ReadSideProcessor[UserEvt]
 	    )
 		"""
 		)
-		//收货地址表
+		//用户收货地址表
 		_ <- session.executeCreateTable(
 			"""
 				CREATE TYPE IF NOT EXISTS address
@@ -84,61 +99,78 @@ extends ReadSideProcessor[UserEvt]
 	} yield Done
 
 	//SQL
-	private def fSQL = for
+	private def fSQLs =
 	{
-	//插入用户
-//		insertUser <- session.prepare(
-//			"""
-//				INSERT INTO user(id,mobile,name,age,create_time,update_time)
-//				VALUES(?,?,{first_name:?,last_name:?,en_name:?},?,?,?)
-//			""")
-		insertUser <- session.prepare("INSERT INTO user(id,mobile,name,age,addresses,create_time,update_time) VALUES(?,?,?,?,?,?,?)")
+		//插入用户
+		val insertUser = session.prepare("INSERT INTO user(id,mobile,name,age,addresses,create_time,update_time) VALUES(?,?,?,?,?,?,?)")
+		insertUserPro.completeWith(insertUser)
 		//删除用户
-		deleteUser <- session.prepare("DELETE FROM user WHERE id = ?")
+		val deleteUser = session.prepare("DELETE FROM user WHERE id = ? IF EXISTS")
+		deleteUserPro.completeWith(deleteUser)
 		//更新用户收货地址
-//		updateAddress <- session
-//		.prepare(
-//			"UPDATE user SET addresses = address + {'?':{province:'?',city:'?',district:'?',zip_code:'?',street:'?',status:'USE',type:'?'}},update_time = ? WHERE id =" +
-//			" ?")
-	} yield
-			{
-				insertUserPre = insertUser
-				deleteUserPre = deleteUser
-//				updateAddressPre = updateAddress
-				Done
-			}
+		val updateAddress = session.prepare("UPDATE user SET addresses = address+?")
+		updateAddressPro.completeWith(updateAddress)
 
-//	UPDATE mykeyspace.users SET addresses = addresses + {'home': { street: '191 Rue St. Charles', city: 'Paris', zip_code: 75015, phones: {'33 6 78 90 12 34'}}} WHERE id=62c36092-82a1-3a00-93d1-46196ee77204;
+		Future(Done)
+	}
 
 	//插入用户
 	private def insertUser(evt: EventStreamElement[Created]) =
 	{
 		println("创建用户")
 		val cmd = evt.event.cmd
-		println(s"$cmd")
+		for
+		{
+			session <- session.underlying().map(_.getCluster.getMetadata.getKeyspace("user"))
+			nameUDT <- Future(session.getUserType("full_name").newValue())
+			list <- insertUserPro.future.map(ps =>
+			{
+				val data = ps.bind
+				data.setString("id", cmd.id)
+				data.setString("mobile", cmd.mobile)
+				cmd.name.map(d =>
+				{
+					nameUDT.setString("first_name",d.firstName)
+					nameUDT.setString("last_name",d.lastName)
+					nameUDT.setString("en_name",d.enName.orNull)
+				})
+				data.setUDTValue("name", nameUDT)
+				data.setInt("age", cmd.age.getOrElse(0))
+				data.setMap[String,Address]("addresses",Map[String,Address]())
+				data.setTimestamp("create_time", cmd.createTime.toDate)
+				data.setTimestamp("update_time", cmd.updateTime.toDate)
 
-		insertUserPre.bind(cmd.id,cmd.mobile,)
-
-//		Future.successful(List(insertUserPre.bind(cmd.id,cmd.mobile,)))
-		Future.successful(List(insertUserPre.bind(cmd.id, cmd.mobile, cmd.name, cmd.age, cmd.createTime, cmd.updateTime)))
-
+				List(data)
+			})
+		} yield list
 	}
 
 	//删除用户
-	private def deleteUser(evt: EventStreamElement[Deleted.type]) = Future
-	.successful(List(deleteUserPre.bind(evt.entityId)))
+	private def deleteUser(evt: EventStreamElement[Deleted.type]) =
+	{
+		println("删除用户")
+		deleteUserPro.future.map(ps => List(ps.bind(evt.entityId)))
+	}
 
 	//更新用户收货地址
 	private def updateAddress(evt: EventStreamElement[CreatedAddress]) =
 	{
 		println("创建用户收货地址")
-
 		val cmd = evt.event.cmd
-		println(s"$cmd")
+		updateAddressPro.future.map(ps =>
+		{
+			val data = ps.bind
+			data.setString("id", cmd.id)
+			data.setString("province", cmd.province)
+			data.setString("city", cmd.city)
+			data.setString("district", cmd.district)
+			data.setInt("zipCode", cmd.zipCode.getOrElse(0))
+			data.setString("street", cmd.street)
+			data.setInt("type", cmd.addressType.id)
+			data.set("update_time", cmd.updateTime, classOf[DateTime])
 
-		Future
-		.successful(List(updateAddressPre.bind(cmd.id, cmd.province, cmd.city, cmd.district, cmd.zipCode, cmd.street, cmd.addressType, cmd.updateTime,
-			cmd.userId)))
+			List(data)
+		})
 	}
 
 }
